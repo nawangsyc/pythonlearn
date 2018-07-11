b6b268be9868da86255fd0882bfb87c65f75c15c
b401 <nawang@csu.edu.cn>
2018-07-11 09:53:16 +0800

    123
    Signed-off-by: b401 <nawang@csu.edu.cn>

---
+++ python-小作业/函数法.py
@@ -0,0 +1,11 @@
+while True:
+    formula = input('请输入公式,公式的变量可以是x或者X：')
+    formula = formula.lower()
+    value = input('你输入的数值是：')
+    value = float(value)
+    x = value
+    print('结果为:', eval(formula))
+    cmd = input("继续吗?是\\否 ")
+    cmd = cmd.lower()
+    if '否' == cmd:
+        break
